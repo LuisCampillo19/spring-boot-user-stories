064668fb9c9c845e57b6955f42e0158ec9b82c25
LuisCampillo19 <lgcampillo19@gmail.com>
2026-05-11 14:00:00 -0500

    feat(model): transform POJOs to JPA entities with @Entity and @Column
    @Entity + @Table, @Id + @GeneratedValue(IDENTITY)
    @Column(nullable=false, length=...) para integridad en BD.

--- src/main/java/com/luiscampillo19/eventify/model/Event.java
+++ src/main/java/com/luiscampillo19/eventify/model/Event.java
@@ -1,5 +1,6 @@
 package com.luiscampillo19.eventify.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,14 +8,24 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "events")
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Event {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 150)
     private String nombre;
+
+    @Column(nullable = false)
     private LocalDate fecha;
+
+    @Column(length = 500)
     private String descripcion;
 }
